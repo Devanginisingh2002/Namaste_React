c31171899de10d05489e152a921a77f7801cbedb
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-07-25 23:45:26 +0530

    lect_4

--- lect_4/app.js
+++ lect_4/app.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import logo from './logo.png';
-
-
+import styleCheck from "./index.css"
 
 const Header = () => {
     return (
         <div className="header">
            <div className="logo-container">
-             <img className="logo" src={logo}></img>
+             <img className="logo" src="https://static.vecteezy.com/system/resources/previews/019/166/318/non_2x/lion-head-lion-logo-symbol-gaming-logo-elegant-element-for-brand-abstract-symbols-vector.jpg">
+             </img>
            </div>
            
            <div className="navbar">
@@ -29,7 +28,10 @@ const AppLayout = () => {
     return (
         <div className="app">
             {/* Header Component */}
-
+            <Header/>
         </div>
     )
 }  
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
